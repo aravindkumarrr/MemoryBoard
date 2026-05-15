b268690527939a13bd5100e1c60fbd04d3055cb6
Aravind Kumar <arkumar7055@gmail.com>
2026-05-15 21:21:46 +0530

    Added font-style icons for simpler, better and faster rendering instead of svgs; Added hover, focus actions; Added change of icons from lock to unlock using useState

--- src/Header.js
+++ src/Header.js
@@ -1,25 +1,53 @@
 import './CSS/Header.css';
+import {useState} from "react";
 
-function Header(){
-    return(
+function Header() {
+    const [locked,setLocked] = useState(true);
+    function toggleLock(){
+        setLocked(!locked);
+    }
+    return (
     <>
-        <div id = "header_container">
-            <div class = "header_button_container">
-                <button id = "lock_btn">Lock</button>
-            </div>
-            <div class = "header_search_bar_container">
-                <input type = "text" id = "header_search_bar" placeholder='Search Memories...'></input>
-            </div>
-            <div class = "header_button_container">
-                <button id = "edit_btn">Edit</button>
-            </div>
-            <div class = "header_button_container">
-                <button id = "add_new_btn">Add New</button>
-            </div>
-            <div class = "header_button_container">
-                <button id = "profile_btn">Profile</button>
-            </div>
-            
+        <div id="header_container">
+
+        <div className="header_button_container">
+            <button id="lock_btn" className="icon_btn" onClick={toggleLock}>
+            {locked ? (<i className="fa-solid fa-lock"></i>) : (<i className="fa-solid fa-unlock"></i>)}
+            </button>
+        </div>
+
+        <div className="header_search_bar_container">
+            <input
+            type="text"
+            id="header_search_bar"
+            placeholder="Search Memories..."
+            />
+        </div>
+
+        <div className="header_button_container">
+            <button id="edit_btn" className="icon_btn">
+            <i className="fa-solid fa-pen"></i>
+            </button>
+        </div>
+
+        <div className="header_button_container">
+            <button id="add_new_btn" className="icon_btn">
+            <i className="fa-solid fa-plus"></i>
+            </button>
+        </div>
+
+        <div className="header_button_container">
+            <button id="profile_btn" className="icon_btn">
+            <i className="fa-solid fa-user"></i>
+            </button>
+        </div>
+
+        <div className="header_button_container">
+            <button id="search_btn" className="icon_btn">
+            <i className="fa-solid fa-magnifying-glass"></i>
+            </button>
+        </div>
+
         </div>
     </>
     );
