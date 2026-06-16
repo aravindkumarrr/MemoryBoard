be865a72c890b20ef21f5cf41fe1705823a2dd3f
Aravind Kumar <arkumar7055@gmail.com>
2026-06-16 21:18:19 +0530

    modified header button spcaing, made it so that it wouldn't shrink on resizing. General frontend part completed.

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,48 +2,42 @@ import './CSS/Header.css';
 
 function Header({ isEditMode, toggleEditMode, onAddClick, onLock }) {
     return (
-    <>
         <div id="header_container">
-            <div className="header_button_container">
-                <button id="lock_btn" className="icon_btn" onClick={onLock}>
-                    <i className="fa-solid fa-unlock"></i>
-                </button>
+            <div className="header_side_group">
+                <div className="header_button_container">
+                    <button id="lock_btn" className="icon_btn" onClick={onLock}>
+                        <i className="fa-solid fa-lock"></i>
+                    </button>
+                </div>
+                <div className="header_button_container">
+                    <button id="profile_btn" className="icon_btn">
+                        <i className="fa-solid fa-user"></i>
+                    </button>
+                </div>
             </div>
 
             <div className="header_search_bar_container">
                 <input type="text" id="header_search_bar" placeholder="Search Memories..." />
             </div>
 
-            <div className="header_button_container">
-                <button 
-                    id="edit_btn" 
-                    className="icon_btn" 
-                    onClick={toggleEditMode}
-                    style={{ background: isEditMode ? 'var(--bg-hover)' : 'transparent', color: isEditMode ? 'var(--accent-blue)' : 'inherit' }}
-                >
-                    <i className="fa-solid fa-pen"></i>
-                </button>
-            </div>
-
-            <div className="header_button_container">
-                <button id="add_new_btn" className="icon_btn" onClick={onAddClick}>
-                    <i className="fa-solid fa-plus"></i>
-                </button>
-            </div>
-
-            <div className="header_button_container">
-                <button id="profile_btn" className="icon_btn">
-                    <i className="fa-solid fa-user"></i>
-                </button>
-            </div>
-
-            <div className="header_button_container">
-                <button id="search_btn" className="icon_btn">
-                    <i className="fa-solid fa-magnifying-glass"></i>
-                </button>
+            <div className="header_side_group">
+                <div className="header_button_container">
+                    <button 
+                        id="edit_btn" 
+                        className="icon_btn" 
+                        onClick={toggleEditMode}
+                        style={{ background: isEditMode ? 'var(--bg-hover)' : 'transparent', color: isEditMode ? 'var(--accent-blue)' : 'inherit' }}
+                    >
+                        <i className="fa-solid fa-pen"></i>
+                    </button>
+                </div>
+                <div className="header_button_container">
+                    <button id="add_new_btn" className="icon_btn" onClick={onAddClick}>
+                        <i className="fa-solid fa-plus"></i>
+                    </button>
+                </div>
             </div>
         </div>
-    </>
     );
 }
 
